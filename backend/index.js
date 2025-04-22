c1f0f4ce826d6fdf34ce44184661d03d0d44ef98
HirenAhir02 <hirenahir509@gmail.com>
2025-04-22 15:19:13 +0530

    3 commit

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,7 @@ const port = process.env.PORT
 //to make input as json
 app.use(express.json())
 app.use(cookieParser())
-app.use(cors({origin: ["http://localhost:5173"] , credentials:true}))
+app.use(cors({origin: ["https://note-tq28.onrender.com"] , credentials:true}))
 
 app.listen(port,()=>{
     console.log(`Server is running on port ${port}`);
